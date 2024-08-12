2fd8d5566e55827d638ee4495c6d2880bcd997c3
Aren Oganesyan <arem.h.y@tumo.org>
2024-08-12 19:15:47 +0400

    Completed registration. Users are saved in text format, and access. To achieve that, user class is now serializable. UI V 1.0 is ready.

--- src/main/java/StartPanel.java
+++ src/main/java/StartPanel.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class StartPanel extends JFrame {
     private boolean switchRegistration = true;
